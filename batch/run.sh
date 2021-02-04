bec0ea30530ff0e35e057075ced4bd56d4e030b8
mam-mih-val <mam.mih.val@gmail.com>
2021-02-05 02:06:25 +0300

    debug partition

--- batch/run.sh
+++ batch/run.sh
@@ -6,8 +6,8 @@ output_dir=$2
 ownroot=/lustre/nyx/hades/user/mmamaev/install/root-6.20.04/bin/thisroot.sh
 
 current_dir=$(pwd)
-partition=main
-time=8:00:00
+partition=debug
+time=0:20:00
 build_dir=/lustre/nyx/hades/user/mmamaev/hades_yield/build/src
 
 lists_dir=${output_dir}/lists/
