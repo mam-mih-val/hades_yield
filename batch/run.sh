58ee4cf05df411ba14b79c232451db19eaca27ab
mam-mih-val <mam.mih.val@gmail.com>
2021-03-15 15:41:39 +0300

    Rewritten according to skeleton 0.6.0

--- batch/run.sh
+++ batch/run.sh
@@ -3,11 +3,11 @@
 file_list=$1
 output_dir=$2
 
-ownroot=/lustre/nyx/hades/user/mmamaev/install/root-6.20.04/bin/thisroot.sh
+ownroot=/lustre/nyx/hades/user/mmamaev/install/root-6.18.04/bin/thisroot.sh
 
 current_dir=$(pwd)
-partition=debug
-time=0:20:00
+partition=main
+time=8:00:00
 build_dir=/lustre/nyx/hades/user/mmamaev/hades_yield/build/src
 
 lists_dir=${output_dir}/lists/
