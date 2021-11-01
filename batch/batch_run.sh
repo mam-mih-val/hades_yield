fc8e1804a8d139d34000c847a756ebc610162558
mam-mih-val <mam.mih.val@gmail.com>
2021-11-01 14:44:45 +0100

    Batch scripts updated

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -22,9 +22,9 @@ module load /cvmfs/vae.gsi.de/centos7/modules/linux-centos7-x86_64/gcc-8.1.0-gcc
 echo "loading " $ownroot
 source $ownroot
 
-echo "executing $build_dir/yield -i list.txt -t hades_analysis_tree -n -1 -o yield.root --cuts-macro Hades/AuAu1.23.C --pdg-code $pdg_code"
+echo "executing $build_dir/yield -i list.txt -t hades_analysis_tree -n -1 -o yield.root --cuts-macro Hades/AuAu1.23.C --pdg-code $pdg_code --efficiency-file /lustre/nyx/hades/user/mmamaev/hades_yield/efficiency/yield_au123_urqmd_2212_2021_10_29.root"
 
-$build_dir/yield -i list.txt -t hades_analysis_tree -n -1 -o yield.root --cuts-macro Hades/AuAu1.23.C --pdg-code $pdg_code
+$build_dir/yield -i list.txt -t hades_analysis_tree -n -1 -o yield.root --cuts-macro Hades/AuAu1.23.C --pdg-code $pdg_code --efficiency-file /lustre/nyx/hades/user/mmamaev/hades_yield/efficiency/yield_au123_urqmd_2212_2021_10_29.root
 
 date $format
 echo JOB FINISHED!
